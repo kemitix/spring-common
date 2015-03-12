ccafcc786296cfa65f9c70744b50784a08e57c83
Paul Campbell <pcampbell@kemitix.net>
2015-03-12 14:19:57 +0000

    JdbcProperties: updated to use @LogField and LoggerProvider
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/spring/common/JdbcProperties.java
+++ src/main/java/net/kemitix/spring/common/JdbcProperties.java
@@ -2,15 +2,22 @@ package net.kemitix.spring.common;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.kemitix.spring.common.logging.LoggableProperties;
+import net.kemitix.spring.common.logging.LogField;
+import net.kemitix.spring.common.logging.LoggerProvider;
 
 @Setter
 @Getter
-public abstract class JdbcProperties extends LoggableProperties {
+public abstract class JdbcProperties implements LoggerProvider {
 
+    @LogField(name = "jdbc driver")
     protected String driver;
+
+    @LogField(name = "jdbc url")
     protected String url;
+
+    @LogField(name = "jdbc user")
     protected String user;
+
     protected String password;
 
 }
